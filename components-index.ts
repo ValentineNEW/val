82863140df970260ce65daa971459a4208fb80a4
sixtusjoe <102814978+sixtusjoe@users.noreply.github.com>
2026-02-08 11:19:19 +0100

    Delete components-index.ts

--- components-index.ts
+++
@@ -1,7 +0,0 @@
-// Barrel export file for cleaner imports
-export { LandingPage } from './LandingPage';
-export { ApplicationForm } from './ApplicationForm';
-export { TransitionPage } from './TransitionPage';
-export { QuestionPage } from './QuestionPage';
-export { CelebrationReveal } from './CelebrationReveal';
-export { GiftBoxAnimation } from './GiftBoxAnimation';
